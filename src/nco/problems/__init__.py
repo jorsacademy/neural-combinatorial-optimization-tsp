b62412d966c6aae33de026b47a0913e0aff6b24d
Jors Academy <abalci16@posta.pau.edu.tr>
2026-08-29 21:53:45 +0300

    feat: export clustered TSP generator

--- src/nco/problems/__init__.py
+++ src/nco/problems/__init__.py
@@ -1,3 +1,13 @@
-from nco.problems.tsp import generate_euclidean_tsp, is_valid_tour, tour_length
+from nco.problems.tsp import (
+    generate_clustered_tsp,
+    generate_euclidean_tsp,
+    is_valid_tour,
+    tour_length,
+)
 
-__all__ = ["generate_euclidean_tsp", "is_valid_tour", "tour_length"]
+__all__ = [
+    "generate_clustered_tsp",
+    "generate_euclidean_tsp",
+    "is_valid_tour",
+    "tour_length",
+]
