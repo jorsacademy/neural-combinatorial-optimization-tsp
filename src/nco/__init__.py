9457b5d9e5b1b0dd14f429f6b56dd019c51b0ea7
Jors Academy <abalci16@posta.pau.edu.tr>
2026-08-29 22:04:15 +0300

    feat: export statistical benchmark helpers

--- src/nco/__init__.py
+++ src/nco/__init__.py
@@ -8,14 +8,18 @@ from nco.problems.tsp import (
     is_valid_tour,
     tour_length,
 )
+from nco.statistics import SummaryStats, normalized_regret, summarize_samples
 
 __all__ = [
+    "SummaryStats",
     "evaluate_batch",
     "generate_clustered_tsp",
     "generate_euclidean_tsp",
     "is_valid_tour",
     "nearest_neighbor",
+    "normalized_regret",
     "optimality_gap",
+    "summarize_samples",
     "tour_length",
     "two_opt",
 ]
